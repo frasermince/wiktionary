c1466e5de4d4d9dc0dfcb1c17685ced4252a4962
Fraser Mince <fraser@thoughtbot.com>
2020-05-27 14:36:38 -0500

    Use different api to fetch words.

--- index.js
+++ index.js
@@ -4,24 +4,17 @@ const {get: getProp} = require('lodash')
 const cheerio = require('react-native-cheerio')
 
 
-async function lookup (query, locale = 'en') {
+async function lookup (query, locale = 'en', language) {
   // Example query
   // https://en.wiktionary.org/w/api.php?action=query&prop=extracts&titles=pomology&format=json
 
   const url = URL.format({
     protocol: 'https',
     hostname: `${locale}.wiktionary.org`,
-    pathname: '/w/api.php',
-    query: {
-      action: 'query',
-      format: 'json',
-      prop: 'extracts',
-      origin: "*",
-      titles: query
-    }
+    pathname: `api/rest_v1/page/definition/${query}`
   })
 
-  const {body} = await fetch(url, 
+  const response = await fetch(url, 
     {
       method: "GET",
       headers: {
@@ -30,19 +23,12 @@ async function lookup (query, locale = 'en') {
       }
     }
   )
-  const key = Object.keys(flat(body)).find(key => key.endsWith('.extract'))
-  if (!key) return null // 404 word not found
-  const html = getProp(body, key)
-  const text = cheerio.load(html).text()
-    .trim() // remove extra whitespace and newlines
-    .replace('English\n', '')
-    .replace('Noun\n', '')
-    .replace('Etymology\n', 'Etymology: ')
-    .replace('Translation\n', 'Translation: ')
-    .replace('Anagrams\n', 'Anagrams: ')
-    .replace(/\n+/gm, '\n') // duplicate newlines
-    .replace(/\n/gm, '; ')
-  return {query, html, text}
+  const body = await response.json()
+  if (body[language] == undefined) {
+    return [];
+  } else {
+    return body[language];
+  }
 }
 
 module.exports = lookup
